5a1a5f18c9d79002cbcac5e013a5e9ba0d73d943
Mist1010 <52849211+Mist1010@users.noreply.github.com>
2020-02-19 21:36:33 -0600

    Initial upload

---
+++ pigLatin_proj.py
@@ -0,0 +1,34 @@
+import random
+
+# Main list sequences, only one word no sentences
+words = ['Horse']
+
+# Find the first letter
+word = random.choice(words)
+let = len(word) - (len(word) - 1)
+first_let = word[:let]
+first_let = first_let.lower()
+
+# Remove first letter
+word_noLet = word[let:]
+
+# Check if letter is vowel
+if first_let == 'a' or first_let == 'e' or first_let == 'i' or first_let == 'o' or first_let == 'u':
+    first_letVow = True
+    confo = ' '
+else:
+    first_letVow = False
+    confo = ' not '
+
+# Convert to pig latin
+if first_letVow == True:
+    reos = word + '-yay'
+elif first_letVow == False:
+    reos = word_noLet + '-' + first_let + 'ay'
+
+# Main Section
+print('Original word: ' + word)
+print('First letter: ' + first_let)
+print('Letter is' + confo + 'a vowel')
+print('\n')
+print('Pig Latin: ' + reos)
